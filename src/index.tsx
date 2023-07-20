19177f902c613d162fd3ce8a4e8568a22be7969d
SKYE <skye@MacBook-Pro-MacBook.local>
2023-07-20 13:25:08 +0300

    Added typescript

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,10 @@ import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
 root.render(
   <BrowserRouter>
     <App />
